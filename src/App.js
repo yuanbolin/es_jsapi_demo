967a066e31890a299086536ca74a41bbbc88e1c2
苑博林 <1252432332@qq.com>
2021-01-05 11:36:08 +0800

    docs(丰富文档): 丰富文档
    丰富文档

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {Client} from '@elastic/elasticsearch'
 
 const client = new Client({node: 'http://192.168.1.162:9200', name: '苑博林'})
 
+// const client = new Client({node: 'http://192.168.14.130:5601', name: '苑博林'})
+
 class App extends Component {
 
     constructor(props) {
@@ -14,10 +16,9 @@ class App extends Component {
     componentDidMount() {
         // this.clientIndex()  //新增索引源
         // this.clientCreate() //新增数据
-        this.clientBulkCreate() //批量新增数据
+        // this.clientBulkCreate() //批量新增数据
         // this.clientSearch()  //条件查询
-        // this.clientSearchId()  //条件查询
-        // this.clientMsearch()  //批量查询
+        // this.clientMsearch()  //批量索引查询
         // this.clientGet()  //id查询
         // this.clientMget()  //批量id查询
         // this.clientMget2()  //批量索引源查询
@@ -28,15 +29,19 @@ class App extends Component {
         // this.clientBulkDelete() //批量删除
     }
 
-    clientIndex=()=>{
+    clientIndex = () => {
         client.index({
             index: 'yuanbolin',
-            id:1,
-            body:{
-                data:new Date(),
-                name:'test',
-                age:24,
-                region:'威海'
+            id: 1,
+            body: {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "web",
+                app_port: "7602",
+                host: "192.168.14.1",
+                level: "INFO",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
             }
         }, (error, response) => {
             if (response) console.log(response)
@@ -46,49 +51,66 @@ class App extends Component {
 
     clientBulkCreate = () => {
         const dataset = [{
-            data:new Date(),
-            name:'json',
-            age:24,
-            region:'威海',
-            location:["25.29999998398125, 55.299999956041574"]
-        }, {
-            data:new Date(),
-            name:'ned',
-            age:24,
-            region:'威海',
-            location:["25.37999998398125, 55.479999956041574"]
-        }, {
-            data:new Date(),
-            name:'tyrion',
-            age:24,
-            region:'淄博',
-            location:["25.12399998398125, 55.435999956041574"]
-        }, {
-            data:new Date(),
-            name:'ndadaed',
-            age:24,
-            region:'淄博',
-            location:["25.65399998398125, 25.99999956041574"]
-        }, {
-            data:new Date(),
-            name:'arya',
-            age:24,
-            region:'青岛',
-            location:["23.29999998398125, 43.299999956041574"]
-        }, {
-            data:new Date(),
-            name:'neasdd',
-            age:24,
-            region:'青岛',
-            location:["65.123998398125, 55.299999956041574"]
-        }, {
-            data:new Date(),
-            name:'yom',
-            age:24,
-            region:'青岛',
-            location:["12.199998398125, 55.299999956041574"]
-        }]
-        const body = dataset.flatMap((doc, index) => [{index: {_index: 'yuanbolin', _id: '121' + index}}, doc])
+            '@timestamp': new Date(),
+            '@version': "1",
+            app_name: "web",
+            app_port: "7602",
+            host: "192.168.1.185",
+            level: "WARN",
+            logger_name: "jsAPI新增",
+            message: "jsAPI新增实例",
+        },
+            {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "app",
+                app_port: "7602",
+                host: "192.168.14.1",
+                level: "ERROR",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
+            },
+            {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "web",
+                app_port: "7602",
+                host: "192.168.14.185",
+                level: "DEBUG",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
+            },
+            {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "app",
+                app_port: "7602",
+                host: "192.168.14.1",
+                level: "DEBUG",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
+            },
+            {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "web",
+                app_port: "7602",
+                host: "192.168.14.185",
+                level: "DEBUG",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
+            },
+            {
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "app",
+                app_port: "7602",
+                host: "192.168.14.1",
+                level: "ERROR",
+                logger_name: "jsAPI新增",
+                message: "jsAPI新增实例",
+            }]
+        const body = dataset.flatMap((doc, index) => [{index: {_index: 'yuanbolin', _id: '2' + index}}, doc])
         console.log(body)
         client.bulk({refresh: true, body}, (error, response) => {
             if (response) console.log(response)
@@ -99,15 +121,17 @@ class App extends Component {
     clientCreate = () => {
         //新增
         client.create({
-            index: 'logback-fengcaiapp-2020.12',
-            type: '_doc',
-            id: '3',
+            index: 'yuanbolin',
+            id: '2',
             body: {
-                app_name: 'Test_2',
-                tags: ['y', 'z'],
-                published: true,
-                '@timestamp': '2013-12-28T02:38:51.716Z',
-                published_at: '2013-01-01', counter: 1
+                '@timestamp': new Date(),
+                '@version': "1",
+                app_name: "app",
+                app_port: "5061",
+                host: "192.168.1.185",
+                level: "DEBUG",
+                logger_name: "jsAPI新增_2",
+                message: "jsAPI新增实例_2",
             }
         }, (error, response) => {
             if (response) console.log(response)
@@ -115,61 +139,40 @@ class App extends Component {
         });
     }
 
-    clientSearchId=()=>{
-        //查询
-        client.search({
-                index: 'yuanbolin*',
-                from: 0, //文档偏移量
-                size: 10, //分页量
-                // q: "app_name:uaa",  //简易查询
-                body: {
-                   aggs:{
-                       termsAgg:{
-                           terms:{
-                               field:"_id"
-                           },
-
-                       }
-                   }
-                }
-            },
-            {
-                ignore: [404],  //HTTP状态码，对于这个请求不应该被认为是错误。
-                requestTimeout:
-                    60000,  //最大请求时长 默认30s
-                maxRetries:
-                    3,  //最大重复请求次数 默认3
-            },
-            (err, result) => {
-                if (result) console.log(result)
-                if (err) console.log(err)
-            }
-        )
-    }
-
 
     clientSearch = () => {
         //查询
         client.search({
-                index: 'logback-*',
-                from: 0, //文档偏移量
-                size: 10, //分页量
-                // q: "app_name:uaa",  //简易查询
+                index: 'yuanbolin',
+                from: 0, //文档偏移量/下标
+                size: 10, //数据量
+                // q: "app_name:web",  //简易查询
                 body: {
                     query: {
                         bool: {
+                            // aggs: {
+                            //     termsAgg: {
+                            //         terms: {
+                            //             field: "_id"
+                            //         },
+                            //     }
+                            // },
                             filter: [   //多参数查询
-                                // {
-                                //     match_phrase: {
-                                //         _id: '1232',
-                                //     }
-                                // },
+                                {
+                                    match_phrase: {
+                                        app_name: 'web',
+                                    }
+                                },
                                 {
                                     range: {
+                                        // 'age': {
+                                        //     gte: "24",
+                                        //     lte: "25"
+                                        // },
                                         '@timestamp': {
                                             format: "strict_date_optional_time",
                                             gte: "2013-12-18T02:38:51.716Z",
-                                            lte: "2020-12-28T03:38:51.716Z"
+                                            lte: "2021-01-05T09:38:51.716Z"
                                         }
                                     }
                                 }
@@ -197,10 +200,25 @@ class App extends Component {
         )
     }
 
+    clientMsearch = () => {
+        client.msearch({
+            body: [
+                {index: 'logback-fengcaiapp-2020.11'},
+                {query: {match: {_id: '1232'}}},
+
+                {index: 'logback-fengcaiapp-2020.12'},
+                {query: {match: {_id: '1'}}}
+            ]
+        }, function (error, response) {
+            if (response) console.log(response)
+            if (error) console.log(error)
+        })
+    }
+
+
     clientGet = () => {
         client.get({
             index: 'logback-fengcaiapp-2020.11',
-            type: '_doc',
             id: '1232',
         }, function (error, response) {
             if (response) console.log(response)
@@ -228,21 +246,6 @@ class App extends Component {
                     _index: 'logback-fengcaiapp-2020.12', _type: '_doc', _id: '1'
                 }]
             }
-        }, function (error, response) {
-            if (response) console.log(response)
-            if (error) console.log(error)
-        })
-    }
-
-    clientMsearch = () => {
-        client.msearch({
-            body: [
-                {index: 'logback-fengcaiapp-2020.11'},
-                {query: {match: {_id: '1232'}}},
-
-                {index: 'logback-fengcaiapp-2020.12'},
-                {query: {match: {_id: '1'}}}
-            ]
         }, function (error, response) {
             if (response) console.log(response)
             if (error) console.log(error)
@@ -292,19 +295,19 @@ class App extends Component {
             text: 'If I fall, don\'t bring me back. yuanbolin',
             user: 'jon',
             date: new Date(),
-            region:'威海'
+            region: '威海'
         }, {
             id: 2,
             text: 'Winter is coming',
             user: 'ned',
             date: new Date(),
-            region:'威海'
+            region: '威海'
         }, {
             id: 3,
             text: 'A Lannister always pays his debts.',
             user: 'tyrion',
             date: new Date(),
-            region:'威海'
+            region: '威海'
         }, {
             id: 4,
             text: 'I am the blood of the dragon.',
@@ -315,21 +318,21 @@ class App extends Component {
             text: 'A girl is Arya Stark of Winterfell. And I\'m going home.',
             user: 'arya',
             date: new Date(),
-            region:'淄博'
+            region: '淄博'
         }, {
             id: 5, // change this value to a string to see the bulk response with errors
             text: 'A girl is Arya Stark of Winterfell. And I\'m going home.',
             user: 'arya',
             date: new Date(),
-            region:'淄博'
+            region: '淄博'
         }, {
             id: 5, // change this value to a string to see the bulk response with errors
             text: 'A girl is Arya Stark of Winterfell. And I\'m going home.',
             user: 'arya',
             date: new Date(),
-            region:'青岛'
+            region: '青岛'
         }]
-        const body = dataset.flatMap((doc,index) => [{index: {_index: 'yuanbolin',_id:'123'+index}}, doc])
+        const body = dataset.flatMap((doc, index) => [{index: {_index: 'yuanbolin', _id: '123' + index}}, doc])
         console.log(body)
         client.bulk({refresh: true, body}, (error, response) => {
             if (response) console.log(response)
@@ -350,8 +353,8 @@ class App extends Component {
     }
 
     clientBulkDelete = () => {
-        const dataset = [1,2,3,4,5]
-        const body = dataset.flatMap((doc,index) => [{delete: {_index: 'yuanbolin',_id:'123'+index}}])
+        const dataset = [1, 2, 3, 4, 5]
+        const body = dataset.flatMap((doc, index) => [{delete: {_index: 'yuanbolin', _id: '123' + index}}])
         console.log(body)
         client.bulk({refresh: true, body}, (error, response) => {
             if (response) console.log(response)
